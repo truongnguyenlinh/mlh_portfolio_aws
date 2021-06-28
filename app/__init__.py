1eb750b26b96fa6ff2667d3c6d648341b6db8d0e
truongnguyenlinh <ltruong28@my.bcit.ca>
2021-06-28 02:30:12 -0700

    Update information and Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,25 @@
 import os
 from flask import Flask, render_template, send_from_directory
 
+# Flask uses load_dotenv by default
+app = Flask(__name__)
 
-def create_app():
+@app.route('/')
+def index():
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
-    # Flask uses load_dotenv by default
-    app = Flask(__name__)
+@app.route("/projects")
+def carousel():
+    return render_template("carousel.html", title="works", url=os.getenv("URL"))
 
-    @app.route('/')
-    def index():
-        return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
-    
-    @app.route("/projects")
-    def carousel():
-        return render_template("carousel.html", title="works", url=os.getenv("URL"))
+@app.route("/about")
+def about_me():
+    return render_template("about_me.html", title="about_me", url=os.getenv("URL"))
 
-    @app.route("/about")
-    def about_me():
-        return render_template("about_me.html", title="about_me", url=os.getenv("URL"))
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", title="Contact", url=os.getenv("URL"))
 
-    @app.route("/contact")
-    def contact():
-        return render_template("contact.html", title="Contact", url=os.getenv("URL"))
-    
-    @app.route("/health")
-    def healthy():
-        return ''
-
-    return app
+@app.route("/health")
+def healthy():
+    return ''
