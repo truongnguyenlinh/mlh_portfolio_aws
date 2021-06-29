f7c3cfe7fb647743e3ba9fbb2f99dfe26dd91340
Cloud User <centos@ip-172-31-6-43.us-west-1.compute.internal>
2021-06-29 04:59:41 +0000

    Add register.html GET and POST requests

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from . import db
+from flask import Flask, request, render_template, request
+from werkzeug.security import generate_password_hash, check_password_hash, generate_password_hash
+from app.db import get_db
+
 
 # Flask uses load_dotenv by default
 app = Flask(__name__)
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
 
 @app.route('/')
 def index():
@@ -23,3 +29,57 @@ def contact():
 @app.route("/health")
 def healthy():
     return ''
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif password is None:
+            error = 'Enter a password to login'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            return "Login Successful", 200 
+        else:
+            return error, 418
+    
+    return render_template("login.html")
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    return render_template("register.html")
