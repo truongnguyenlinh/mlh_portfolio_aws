d75fe41b09ee5987ca567aa35e60ed6cbe8beea6
truongnguyenlinh <ltruong28@my.bcit.ca>
2021-07-12 02:19:10 -0700

    Update paths-ignore and fix lint errors

--- app/__init__.py
+++ app/__init__.py
@@ -7,22 +7,23 @@ from flask_migrate import Migrate
 
 # Flask uses load_dotenv by default
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-     'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
-    user=os.getenv('POSTGRES_USER'),
-    passwd=os.getenv('POSTGRES_PASSWORD'),
-    host=os.getenv('POSTGRES_HOST'),
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+    user=os.getenv("POSTGRES_USER"),
+    passwd=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
     port=5432,
-    table=os.getenv('POSTGRES_DB'))
+    table=os.getenv("POSTGRES_DB"))
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     username = db.Column(db.String(), primary_key=True)
     password = db.Column(db.String())
@@ -35,45 +36,49 @@ class UserModel(db.Model):
         return f"<User {self.username}>"
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template("index.html", title="MLH Fellow",
+                           url=os.getenv("URL"))
 
 
 @app.route("/projects")
 def carousel():
-    return render_template("carousel.html", title="works", url=os.getenv("URL"))
+    return render_template("carousel.html", title="works",
+                           url=os.getenv("URL"))
 
 
 @app.route("/about")
 def about_me():
-    return render_template("about_me.html", title="about_me", url=os.getenv("URL"))
+    return render_template("about_me.html", title="about_me",
+                           url=os.getenv("URL"))
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html", title="Contact", url=os.getenv("URL"))
+    return render_template("contact.html", title="Contact",
+                           url=os.getenv("URL"))
 
 
 @app.route("/health")
 def healthy():
-    return ''
+    return ""
 
 
-@app.route('/login', methods=('GET', 'POST'))
+@app.route("/login", methods=("GET", "POST"))
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
         user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
-            error = 'Incorrect username.'
+            error = "Incorrect username."
         elif password is None:
-            error = 'Enter a password to login'
+            error = "Enter a password to login"
         elif not check_password_hash(user.password, password):
-            error = 'Incorrect password.'
+            error = "Incorrect password."
 
         if error is None:
             return "Login Successful", 200
@@ -83,17 +88,17 @@ def login():
     return render_template("login.html")
 
 
-@app.route('/register', methods=('GET', 'POST'))
+@app.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
 
         if not username:
-            error = 'Username is required.'
+            error = "Username is required."
         elif not password:
-            error = 'Password is required.'
+            error = "Password is required."
         elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
